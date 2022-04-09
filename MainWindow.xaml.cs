fc47c869ceb23f8d2b9696eaf540360cce3a2eb4
Lucas Beeman <Lucasbeeman12@gmail.com>
2022-04-09 13:20:53 -0400

    lets see what happens

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,26 +15,60 @@ using System.Windows.Shapes;
 
 namespace TestSave
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
+        Random rnd = new Random();
+        private string characters = "abcdefghijklmnopqrstuvwqyz";
+        private string capCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public string numerals = "1234567890";
+        public string special = "!@#$%^&*()_-+=?";
+        public string selectionList = "";
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void MainWindow_Load(object sender, RoutedEventArgs e)
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
         {
-            nameLabel.Text = Properties.Settings.Default.textboxValue;
+            MessageBox.Show("copied");
         }
 
-        private void saveButton_Click(object sender, RoutedEventArgs e)
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
         {
-            nameLabel.Text = nameBox.Text;
-            Properties.Settings.Default.textboxValue = nameLabel.Text;
-            Properties.Settings.Default.Save();
+            string password = "";
+            string selectionList = characters;
+            if (isNumeric.IsChecked == true)
+                selectionList += numerals;
+            if (isSpecial.IsChecked == true)
+                selectionList += special;
+            if (isCapital.IsChecked == true)
+                selectionList += capCharacters;
+            try
+            {
+                int.Parse(numOfChars.Text);
+            }
+            catch
+            {
+                numOfChars.Text = "0";
+            }
+            if (int.Parse(numOfChars.Text) <= 0)
+                MessageBox.Show("You must enter a number greater than 0!");
+            else
+            {
+                for (int i = 0; i < int.Parse(numOfChars.Text); i++)
+                    password += selectionList[rnd.Next(selectionList.Length)];
+                list.Items.Add($"Service: {serviceName.Text}, User: {userName.Text}, password: {password}");
+            }
+            serviceName.Text = "";
+            userName.Text = "";
+            numOfChars.Text = "";
+            password = "";
+            
         }
     }
 }
